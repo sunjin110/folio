2fcd85cccd1d0e29b3c25c443a705c18408385df
sunjin110 <sunjin110@gmail.com>
2024-04-22 22:21:32 +0800

    a

--- reolio/src/lib/google/oauth.ts
+++ reolio/src/lib/google/oauth.ts
@@ -15,14 +15,6 @@ export function createOAuth2Client(options?: {clientId?: string, clientSecret?: 
     return new google.auth.OAuth2(clientId, clientSecret, redirectUri);
 }
 
-// const generateOAuth2Url:  = () => {
-//     return "";
-// };
-
-// https://accounts.google.com/o/oauth2/v2/auth?client_id=682633467318-vvlia00uaag3jplkls0uj1md371k54as.apps.googleusercontent.com&
-// redirect_uri=http://localhost:3001/auth/google-oauth/callback&
-// response_type=code&scope=profile email&access_type=offline
-
 interface QueryParams {
     client_id?: string;
     redirect_uri?: string;
@@ -31,56 +23,23 @@ interface QueryParams {
     access_type?: string;
 }
 
-export function generateOAuth2Url(): string {
-    const queryParams: QueryParams = {
-        client_id: process.env.GOOGLE_OAUTH_CLIENT_ID,
-        redirect_uri: process.env.GOOGLE_OAUTH_REDIRECT_URL,
-        response_type: 'code',
-        scope: 'profile email',
-        access_type: 'offline', // TODO localのみ
-    };
+// export function generateOAuth2Url(): string {
+//     const queryParams: QueryParams = {
+//         client_id: process.env.GOOGLE_OAUTH_CLIENT_ID,
+//         redirect_uri: process.env.GOOGLE_OAUTH_REDIRECT_URL,
+//         response_type: 'code',
+//         scope: 'profile email',
+//         access_type: 'offline', // TODO localのみ
+//     };
 
-    const url = new URL(process.env.GOOGLE_OAUTH_URL);
+//     const url = new URL(process.env.GOOGLE_OAUTH_URL);
 
-    Object.keys(queryParams).forEach(key => {
-        const value = queryParams[key as keyof QueryParams];
-        if (value !== undefined) {
-            url.searchParams.append(key, value.toString());
-        }
-    });
-
-    return url.toString();
-}
-
-//   // HTTP GET リクエストを行う非同期関数
-//   async function fetchResource(url: string, queryParams: QueryParams): Promise<any> {
-//     const urlObject = new URL(url);
-  
-//     // クエリパラメータを動的に追加
 //     Object.keys(queryParams).forEach(key => {
-//       const value = queryParams[key as keyof QueryParams];
-//       if (value !== undefined) { // undefined のパラメータは追加しない
-//         urlObject.searchParams.append(key, value.toString());
-//       }
+//         const value = queryParams[key as keyof QueryParams];
+//         if (value !== undefined) {
+//             url.searchParams.append(key, value.toString());
+//         }
 //     });
-  
-//     try {
-//       const response = await fetch(urlObject.toString());
-//       if (!response.ok) {
-//         throw new Error('Network response was not ok');
-//       }
-//       return response.json(); // レスポンスボディをJSONとしてパース
-//     } catch (error) {
-//       console.error('Failed to fetch resource:', error);
-//       throw error;
-//     }
-//   }
-  
-//   // 関数の使用例
-//   fetchResource('https://jsonplaceholder.typicode.com/posts', {
-//     userId: 10,
-//     limit: 5,
-//     sortBy: 'date',
-//     asc: true
-//   }).then(data => console.log(data));
-  
+
+//     return url.toString();
+// }
--- reolio/src/pages/login.tsx
+++ reolio/src/pages/login.tsx
@@ -1,16 +1,22 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+// import { generateOAuth2Url } from "@/lib/google/oauth";
 import React from "react";
 import { Link } from "react-router-dom";
 
 const Login: React.FC = () => {
+
+    // const oauth2Url = generateOAuth2Url();
+    // console.log("oauth2url is ", oauth2Url);
+
+
     return (
         <Card className="w-[350px]">
             <CardHeader>
                 <CardTitle>Login</CardTitle>
             </CardHeader>
             <CardContent>
-                <a href="">
+                <a href={"/test"}>
                     <Button>Google Login</Button>
                 </a>
             </CardContent>
