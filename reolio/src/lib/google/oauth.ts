d09172a76c6773142f15e57d8e235a058fb80388
sunjin110 <sunjin110@gmail.com>
2024-04-22 13:40:19 +0800

    a

--- reolio/src/lib/google/oauth.ts
+++ reolio/src/lib/google/oauth.ts
@@ -1,10 +1,16 @@
+import { google } from "googleapis";
 
 export const OPTIONS = {
     clientId: process.env.GOOGLE_OAUTH_CLIENT_ID,
     clientSecret: process.env.GOOGLE_OAUTH_SECRET_ID,
-    redirectUrl: process.env.GOOGLE_OAUTH_REDIRECT_URL,
+    redirectUri: process.env.GOOGLE_OAUTH_REDIRECT_URL,
 };
 
 export function createOAuth2Client(options?: {clientId?: string, clientSecret?: string, redirectUri?: string}) {
+    const { clientId, clientSecret, redirectUri } = {
+        ...OPTIONS,
+        ...options,
+    };
 
+    return new google.auth.OAuth2(clientId, clientSecret, redirectUri);
 }
