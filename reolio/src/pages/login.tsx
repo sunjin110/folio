c871da40c98140aa786fb746e22f04539bbd56fc
sunjin110 <sunjin110@gmail.com>
2024-04-22 16:27:53 +0800

    a

--- reolio/src/pages/login.tsx
+++ reolio/src/pages/login.tsx
@@ -1,7 +1,16 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 const Login: React.FC = () => {
-    return <h2>Login</h2>;
+    // return <h2>Login</h2>;
+    return (
+        <div>
+            <h2>Login</h2>
+            <Button>
+                Hello World
+            </Button>
+        </div>
+    );
 };
 
 export default Login;
