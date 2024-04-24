97beee78726342eb419626994e8b0dd5820d67f9
sunjin110 <sunjin110@gmail.com>
2024-04-24 08:42:11 +0800

    a

--- golio/go.mod
+++ golio/go.mod
@@ -3,7 +3,9 @@ module github.com/sunjin110/folio/golio
 go 1.22.2
 
 require (
+	github.com/caarlos0/env/v10 v10.0.0
 	github.com/gorilla/mux v1.8.1
+	github.com/joho/godotenv v1.5.1
 	github.com/smartystreets/goconvey v1.8.1
 )
 
--- golio/presentation/http/httpconf/httpconf.go
+++ golio/presentation/http/httpconf/httpconf.go
@@ -1,4 +1,27 @@
 package httpconf
 
+import (
+	"fmt"
+
+	"github.com/caarlos0/env/v10"
+	"github.com/joho/godotenv"
+)
+
 type Config struct {
+	GoogleOAuth *GoogleOAuthConfig `envPrefix:"GOOGLE_OAUTH_"`
+}
+
+type GoogleOAuthConfig struct {
+	ClientID     string `env:"CLIENT_ID"`
+	ClientSecret string `env:"CLIENT_SECRET"`
+	RedirectURI  string `env:"REDIRECT_URI"`
+}
+
+func NewConfig() (*Config, error) {
+	_ = godotenv.Load(".env")
+	cfg := &Config{}
+	if err := env.Parse(cfg); err != nil {
+		return nil, fmt.Errorf("fialed to parse basic config: %w", err)
+	}
+	return cfg, nil
 }
