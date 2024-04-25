72be4971d0a7c50a3a55631408887888f87475f2
sunjin110 <sunjin110@gmail.com>
2024-04-25 20:50:27 +0800

    a

--- golio/infrastructure/repository/authorization.go
+++ golio/infrastructure/repository/authorization.go
@@ -1,33 +1,57 @@
 package repository
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
+
+	"text/template"
 
 	"github.com/sunjin110/folio/golio/domain/model"
 	"github.com/sunjin110/folio/golio/domain/repository"
+	"github.com/sunjin110/folio/golio/infrastructure/repository/dto"
 )
 
 // https://developers.cloudflare.com/api/
 const cloudflareAPIEndpoint = "https://api.cloudflare.com/client/v4"
 
 const (
-	readKVPairPath = "/accounts/{:account_id}/storage/kv/namespaces/{:namespace_id}/values/{:key_name}"
+	readKVPairPath  = "/accounts/{{.AccountID}}/storage/kv/namespaces/{{.NamespaceID}}/values/{{.KeyName}}"
+	writeKVPairPath = "/accounts/{{.AccountID}}/storage/kv/namespaces/{{.NamespaceID}}/values/{{.KeyName}}"
 )
 
 // https://developers.cloudflare.com/api/operations/workers-kv-namespace-list-namespaces
 type authorizationKVStore struct {
-	apiToken    string
-	namespaceID string
-	client      *http.Client
+	apiToken           string
+	accountID          string
+	namespaceID        string
+	client             *http.Client
+	readKVPairPathTmp  *template.Template
+	writeKVPairPathTmp *template.Template
 }
 
-func NewAuthorizationKVStore(ctx context.Context, apiToken string, namespaceID string) repository.Authorization {
-	return &authorizationKVStore{
-		apiToken:    apiToken,
-		namespaceID: namespaceID,
-		client:      &http.Client{},
+func NewAuthorizationKVStore(ctx context.Context, apiToken string, accountID string, namespaceID string) (repository.Authorization, error) {
+	readKVPairPathTmp, err := template.New("read_kv_pair_path").Parse(readKVPairPath)
+	if err != nil {
+		return nil, fmt.Errorf("failed make readKVPairPathTmp: %w", err)
 	}
+
+	writeKVPairPathTmp, err := template.New("writer_kv_pair_path").Parse(writeKVPairPath)
+	if err != nil {
+		return nil, fmt.Errorf("failed make writeKVPairPathTmp: %w", err)
+	}
+
+	return &authorizationKVStore{
+		apiToken:           apiToken,
+		accountID:          accountID,
+		namespaceID:        namespaceID,
+		client:             &http.Client{},
+		readKVPairPathTmp:  readKVPairPathTmp,
+		writeKVPairPathTmp: writeKVPairPathTmp,
+	}, nil
 }
 
 // CloseSession implements repository.Authorization.
@@ -38,7 +62,40 @@ func (a *authorizationKVStore) CloseSession(ctx context.Context, accessToken str
 // Get implements repository.Authorization.
 // Subtle: this method shadows the method (*Client).Get of authorizationKVStore.Client.
 func (a *authorizationKVStore) Get(ctx context.Context, accessToken string) (*model.UserAuthorization, error) {
-	panic("unimplemented")
+
+	pathBuf := &bytes.Buffer{}
+	a.readKVPairPathTmp.Execute(pathBuf, &dto.AuthorizationGetInput{
+		AccountID:   a.accountID,
+		NamespaceID: a.namespaceID,
+		AccessToken: accessToken,
+	})
+
+	url := cloudflareAPIEndpoint + pathBuf.String()
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("http.NewRequest: url: %s, err: %w", url, err)
+	}
+	req = req.WithContext(ctx)
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", a.apiToken))
+	req.Header.Set("ContentType", "application/json")
+
+	resp, err := a.client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed a.client. url: %s, err: %w", url, err)
+	}
+	defer resp.Body.Close()
+
+	respBuf := &bytes.Buffer{}
+	if _, err := io.ReadAll(respBuf); err != nil {
+		return nil, fmt.Errorf("failed io.ReadAll. err: %w", err)
+	}
+
+	userAuthorization := &dto.AuthorizationKVValue{}
+	if err := json.Unmarshal(respBuf.Bytes(), userAuthorization); err != nil {
+		return nil, fmt.Errorf("failed json.Unmarshal. err: %w", err)
+	}
+	return userAuthorization.ToModel(), nil
 }
 
 // StartSession implements repository.Authorization.
--- golio/infrastructure/repository/dto/authorization.go
+++ golio/infrastructure/repository/dto/authorization.go
@@ -1,9 +1,28 @@
 package dto
 
+import "github.com/sunjin110/folio/golio/domain/model"
+
 // AuthorizationKVValue CloudFlare kvのvalue
 type AuthorizationKVValue struct {
 	Email       string `json:"email"`
 	FirstName   string `json:"first_name"`
 	LastName    string `json:"last_name"`
 	AccessToken string `json:"access_token"`
+}
+
+func (dto *AuthorizationKVValue) ToModel() *model.UserAuthorization {
+	if dto == nil {
+		return nil
+	}
+	return &model.UserAuthorization{
+		Email:     dto.Email,
+		FirstName: dto.FirstName,
+		LastName:  dto.LastName,
+	}
+}
+
+type AuthorizationGetInput struct {
+	AccountID   string
+	NamespaceID string
+	AccessToken string
 }
