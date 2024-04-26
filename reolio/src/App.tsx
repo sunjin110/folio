ef7d501d336259993c824f11c88c1ea2275ba0f4
sunjin110 <sunjin110@gmail.com>
2024-04-26 11:40:11 +0800

    a

--- reolio/src/App.tsx
+++ reolio/src/App.tsx
@@ -5,31 +5,36 @@ import Home from './pages';
 import About from './pages/about';
 import Login from './pages/login';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import Dashboard from './pages/dashboard';
+import { TooltipProvider } from '@radix-ui/react-tooltip';
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <div>
-      <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path='/login' element={<Login />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <TooltipProvider>
+      <BrowserRouter>
+        <div>
+        {/* <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+            </ul>
+          </nav> */}
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/dashboard' element={<Dashboard />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </TooltipProvider>
   );
 };
 
