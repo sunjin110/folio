6ebd147c95125164e8c9e30aad1526c97ede2c71
sunjin110 <sunjin110@gmail.com>
2024-04-26 13:00:23 +0800

    a

--- reolio/src/pages/login.tsx
+++ reolio/src/pages/login.tsx
@@ -41,15 +41,16 @@ export default function Login() {
         </CardHeader>
         <CardContent>
             <div className="grid gap-4">
-            <Button type="submit" className="w-full">
-                <div className="mr-4">
-                    <FaGoogle /> 
-                </div>
-                <div className="">
-                    Sign up with Google
-                </div>
-                
-            </Button>
+            <a href={process.env.REACT_APP_GOLIO_BASE_URL + "/auth/google-oauth"}>
+                <Button type="submit" className="w-full">
+                    <div className="mr-4">
+                        <FaGoogle /> 
+                    </div>
+                    <div className="">
+                        Sign up with Google
+                    </div>
+                </Button>
+            </a>
             <Button variant="outline" className="w-full">
                 Sign up with GitHub
             </Button>
