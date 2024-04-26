77bbf662c8ae24908cf926a404cb4faa5fb16b87
sunjin110 <sunjin110@gmail.com>
2024-04-26 12:58:56 +0800

    a

--- reolio/README.md
+++ reolio/README.md
@@ -2,3 +2,8 @@
 
 ## style
 https://ui.shadcn.com/docs/components/accordion
+
+
+## icons
+
+https://react-icons.github.io/react-icons/
--- reolio/src/pages/login.tsx
+++ reolio/src/pages/login.tsx
@@ -27,57 +27,41 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+import { FaGoogle } from "react-icons/fa";
 
 export default function Login() {
   return (
-    <Card className="mx-auto max-w-sm">
-      <CardHeader>
-        <CardTitle className="text-xl">Sign Up</CardTitle>
-        <CardDescription>
-          Enter your information to create an account
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <div className="grid gap-4">
-          <div className="grid grid-cols-2 gap-4">
-            <div className="grid gap-2">
-              <Label htmlFor="first-name">First name</Label>
-              <Input id="first-name" placeholder="Max" required />
+    <div className="flex items-center justify-center min-h-screen">
+        <Card className="max-w-sm">
+        <CardHeader>
+            <CardTitle className="text-xl">Sign Up</CardTitle>
+            <CardDescription>
+            Enter your information to create an account
+            </CardDescription>
+        </CardHeader>
+        <CardContent>
+            <div className="grid gap-4">
+            <Button type="submit" className="w-full">
+                <div className="mr-4">
+                    <FaGoogle /> 
+                </div>
+                <div className="">
+                    Sign up with Google
+                </div>
+                
+            </Button>
+            <Button variant="outline" className="w-full">
+                Sign up with GitHub
+            </Button>
             </div>
-            <div className="grid gap-2">
-              <Label htmlFor="last-name">Last name</Label>
-              <Input id="last-name" placeholder="Robinson" required />
+            <div className="mt-4 text-center text-sm">
+            Already have an account?{" "}
+            <a href="#" className="underline">
+                Sign in
+            </a>
             </div>
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              id="email"
-              type="email"
-              placeholder="m@example.com"
-              required
-            />
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="password">Password</Label>
-            <Input id="password" type="password" />
-          </div>
-          <Button type="submit" className="w-full">
-            Create an account
-          </Button>
-          <Button variant="outline" className="w-full">
-            Sign up with GitHub
-          </Button>
-        </div>
-        <div className="mt-4 text-center text-sm">
-          Already have an account?{" "}
-          <a href="#" className="underline">
-            Sign in
-          </a>
-        </div>
-      </CardContent>
-    </Card>
+        </CardContent>
+        </Card>
+    </div>
   )
 }
