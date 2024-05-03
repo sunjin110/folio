7a463e90c9b37d4ac1c54e6a30f98fccfb459bde
sunjin110 <sunjin110@gmail.com>
2024-05-03 13:05:49 +0800

    feat: terraform format github actions

--- terraform/modules/kv_store/main.tf
+++ terraform/modules/kv_store/main.tf
@@ -1,13 +1,13 @@
 terraform {
   required_providers {
     cloudflare = {
-        source = "cloudflare/cloudflare"
-        version = "~> 4"
+      source  = "cloudflare/cloudflare"
+      version = "~> 4"
     }
   }
 }
 
 resource "cloudflare_workers_kv_namespace" "this" {
-    account_id = var.cloudflare_account_id
-    title = var.name
+  account_id = var.cloudflare_account_id
+  title      = var.name
 }
