02664d0d57aeff24dac12ab40845d55343711261
sunjin110 <sunjin110@gmail.com>
2024-05-03 16:14:34 +0800

    feat: lambdaのための実装

--- golio/presentation/http/http.go
+++ golio/presentation/http/http.go
@@ -6,7 +6,7 @@ import (
 	"log/slog"
 	"net/http"
 
-	"github.com/rs/cors"
+	"github.com/gorilla/mux"
 	"github.com/sunjin110/folio/golio/generate/schema/http/go/openapi"
 	"github.com/sunjin110/folio/golio/infrastructure/cloudflare/d1"
 	"github.com/sunjin110/folio/golio/infrastructure/repository"
@@ -14,21 +14,21 @@ import (
 	"github.com/sunjin110/folio/golio/usecase"
 )
 
-func Serve(ctx context.Context, cfg *httpconf.Config) error {
+func Router(ctx context.Context, cfg *httpconf.Config) (*mux.Router, error) {
 	googleOAuth2Repo := repository.NewGoogleOAuth2(ctx, cfg.GoogleOAuth.ClientID, cfg.GoogleOAuth.ClientSecret, cfg.GoogleOAuth.RedirectURI)
 	sessionRepo, err := repository.NewSessionKVStore(ctx, cfg.SessionKVStore.APIToken, cfg.SessionKVStore.AccountID, cfg.SessionKVStore.NamespaceID)
 	if err != nil {
-		return fmt.Errorf("failed repository.NewSessionKVStore: %w", err)
+		return nil, fmt.Errorf("failed repository.NewSessionKVStore: %w", err)
 	}
 
 	d1Client, err := d1.NewClient(cfg.D1Database.AccountID, cfg.D1Database.DatabaseID, cfg.D1Database.APIToken)
 	if err != nil {
-		return fmt.Errorf("failed d1.NewClient: %w", err)
+		return nil, fmt.Errorf("failed d1.NewClient: %w", err)
 	}
 
 	articleRepo, err := repository.NewArticle(ctx, d1Client)
 	if err != nil {
-		return fmt.Errorf("failed repository.NewArticle: %w", err)
+		return nil, fmt.Errorf("failed repository.NewArticle: %w", err)
 	}
 
 	authUsecase := usecase.NewAuth(googleOAuth2Repo, sessionRepo)
@@ -50,20 +50,19 @@ func Serve(ctx context.Context, cfg *httpconf.Config) error {
 		HandlerFunc(googleOAuthController.Callback)
 
 	// middleware
-	r.Use(authMW(authUsecase))
+	r.Use(AuthMW(authUsecase))
+	r.Use(CorsMW())
+	return r, nil
+}
 
-	// CORSミドルウェアの設定
-	// すべてのオリジンからのアクセスを許可する設定
-	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:3000"}, // staging, productionのoriginも設定する
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"Accept", "Content-Type", "Content-Length", "Accept-Encoding", "X-CSRF-Token", "Authorization"},
-		AllowCredentials: true,
-	})
-	handler := c.Handler(r)
+func Serve(ctx context.Context, cfg *httpconf.Config) error {
+	r, err := Router(ctx, cfg)
+	if err != nil {
+		return fmt.Errorf("failed Router: %w", err)
+	}
 
 	slog.Info("server started", "port", cfg.Server.PORT)
-	if err := http.ListenAndServe(cfg.Server.PORT, handler); err != nil {
+	if err := http.ListenAndServe(cfg.Server.PORT, r); err != nil {
 		return fmt.Errorf("failed http.ListenAndServe: %w", err)
 	}
 	return nil
