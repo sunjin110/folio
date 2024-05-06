6d8a5e1179459fb74c9a626cb31bd67e35750fcf
sunjin110 <sunjin110@gmail.com>
2024-05-06 15:56:46 +0800

    fix: cookieが渡らない問題を解決する

--- reolio/src/pages/login.tsx
+++ reolio/src/pages/login.tsx
@@ -1,3 +1,4 @@
+import { Navigation } from "@/components/organisms/navigation";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -10,38 +11,34 @@ import { FaGoogle } from "react-icons/fa";
 
 export default function Login() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <Card className="max-w-sm">
-        <CardHeader>
-          <CardTitle className="text-xl">Sign In</CardTitle>
-          <CardDescription>
-            Enter your information to create an account
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <div className="grid gap-4">
-            <a
-              href={process.env.REACT_APP_GOLIO_BASE_URL + "/auth/google-oauth"}
-            >
-              <Button type="submit" className="w-full">
-                <div className="mr-4">
-                  <FaGoogle />
-                </div>
-                <div className="">Sign up with Google</div>
+    <Navigation title="Login" sidebarPosition="">
+      <div className="flex items-center justify-center min-h-screen">
+        <Card className="max-w-sm">
+          <CardHeader>
+            <CardTitle className="text-xl">Sign In</CardTitle>
+            <CardDescription>
+              Enter your information to create an account
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="grid gap-4">
+              <a
+                href={process.env.REACT_APP_GOLIO_BASE_URL + "/auth/google-oauth"}
+              >
+                <Button type="submit" className="w-full">
+                  <div className="mr-4">
+                    <FaGoogle />
+                  </div>
+                  <div className="">Sign up with Google</div>
+                </Button>
+              </a>
+              <Button variant="outline" className="w-full">
+                Sign up with GitHub TODO
               </Button>
-            </a>
-            <Button variant="outline" className="w-full">
-              Sign up with GitHub TODO
-            </Button>
-          </div>
-          {/* <div className="mt-4 text-center text-sm">
-            Already have an account?{" "}
-            <a href="#" className="underline">
-                Sign in
-            </a>
-            </div> */}
-        </CardContent>
-      </Card>
-    </div>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    </Navigation>
   );
 }
