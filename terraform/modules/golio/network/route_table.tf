cec64ac0f42cdd7f555a8653cde5c4f66f2847bd
sunjin110 <sunjin110@gmail.com>
2024-05-19 23:09:04 +0800

    feat: nat gatewayを定義

---
+++ terraform/modules/golio/network/route_table.tf
@@ -0,0 +1,70 @@
+resource "aws_route_table" "private_route_table_1" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway_1.id
+  }
+
+  tags = {
+    Name = "${var.prefix}-golio-nat-gateway-route-table-1"
+  }
+}
+
+resource "aws_route_table" "private_route_table_2" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway_2.id
+  }
+
+  tags = {
+    Name = "${var.prefix}-golio-nat-gateway-route-table-2"
+  }
+}
+
+resource "aws_route_table_association" "private_route_table_assoc_1" {
+  subnet_id      = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.private_route_table_1.id
+}
+
+resource "aws_route_table_association" "private_route_table_assoc_2" {
+  subnet_id      = aws_subnet.private_subnet_2.id
+  route_table_id = aws_route_table.private_route_table_2.id
+}
+
+
+resource "aws_route_table" "public_route_table_1" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+  tags = {
+    Name = "${var.prefix}-golio-internet-gateway-route-table-1"
+  }
+}
+
+resource "aws_route_table" "public_route_table_2" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+  tags = {
+    Name = "${var.prefix}-golio-internet-gateway-route-table-2"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_assoc_1" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_route_table_1.id
+}
+
+resource "aws_route_table_association" "public_subnet_assoc_2" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_route_table_2.id
+}
