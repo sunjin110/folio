5417f926c7a48e2625bfbb678a529e5c18b8072b
sunjin110 <sunjin110@gmail.com>
2024-06-10 13:25:00 +0800

    refactor: chatgpt呼び出しまわり

---
+++ golio/infrastructure/chatgpt/dto/dto.go
@@ -0,0 +1,4 @@
+package dto
+
+// M どうしてもjsonで表現する必要がある場合をケアする
+type M map[string]any
--- golio/infrastructure/repository/articlev2_test.go
+++ golio/infrastructure/repository/articlev2_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/sunjin110/folio/golio/infrastructure/chatgpt"
 	"github.com/sunjin110/folio/golio/infrastructure/gcp/custom_search_api"
 	"github.com/sunjin110/folio/golio/infrastructure/repository"
+	"github.com/sunjin110/folio/golio/presentation/http/httpconf"
 )
 
 // go test -v -count=1 -timeout 30s -run ^Test_articleV2_ChangeBodyByAI_Real$ github.com/sunjin110/folio/golio/infrastructure/repository
@@ -70,10 +71,13 @@ func Test_articleV2_ChangeBodyByAI_Real(t *testing.T) {
 func Test_articleV2_GenerateBodyByAI_Real(t *testing.T) {
 	SkipConvey("Test_articleV2_GenerateBodyByAI_Real", t, func() {
 
-		ctx := context.Background()
-		client := chatgpt.NewClient("")
+		conf, err := httpconf.NewConfig()
+		So(err, ShouldBeNil)
 
-		customSearchClient, err := custom_search_api.NewClient(ctx, "")
+		ctx := context.Background()
+		client := chatgpt.NewClient(conf.ChatGPT.APIKey)
+
+		customSearchClient, err := custom_search_api.NewClient(ctx, conf.GoogleCustomSearchKey)
 		So(err, ShouldBeNil)
 
 		googleCustomSearchRepo := repository.NewGoogleCustomSearch(customSearchClient)
@@ -83,43 +87,5 @@ func Test_articleV2_GenerateBodyByAI_Real(t *testing.T) {
 		result, err := articleV2Repo.GenerateBodyByAI(ctx, "今日の天気を教えてください")
 		So(err, ShouldBeNil)
 		fmt.Println("result is ", result)
-
-		// 		_, err = articleV2Repo.ChangeBodyByAI(ctx, &model.Article{
-		// 			Body: `
-		// ## phrase
-
-		// - But it is even more difficult when it is done in what is known as "alpine style"
-		// - Himalayas were being climbed.
-		// - They would then climb their way from one camp to the next
-		// - It took them only three days to reach toe top of montain.
-		// - It took them only three days to ~
-
-		// ## words
-		// - alternative: 別の
-		// - siege: 包囲
-		// - dozens: 数十
-		// - series of camps equipped
-		// - supplies: 用品
-		// - eventually: 最終的に
-		// - hire: 雇う
-		// - **set out: 出発する**
-		// - Gasherbrum: 山の名前
-		// - in this way: このように
-		// - to took me {xxx days} to ~
-		// - the first ever: 史上初めて
-		// - Since it takes less time: 時間があまりかからないので
-		// 	- Sinceは~なのでという意味で使われることもある: ニュアンスとしてはすでに明確でわかっていること
-		// - being caught: 捕まる
-		// - avalanche: 雪雪崩
-		// - there is a smaller chance of ~ : ~の可能性は低い
-		// - expeditions: 遠征
-		// - trace: 痕跡
-		// - accomplishments: 成果
-		// -
-		// `,
-		// 		}, "phraseの欄に日本語翻訳を追加してください、元の英文の右に日本語翻訳を付与してください")
-
-		// 		So(err, ShouldBeNil)
-
 	})
 }
