c42265757c39468c6432a245ac4c0e5f46e8003c
sunjin110 <sunjin110@gmail.com>
2024-06-11 11:03:42 +0800

    Revert "a"
    This reverts commit 05585b66c192c36e27a540994f2fe5f6c94f9bfd.

--- golio/domain/model/html.go
+++ golio/domain/model/html.go
@@ -12,13 +12,11 @@ import (
 
 type HtmlExtractor struct {
 	tokenizer *html.Tokenizer
-	baseURL   string // https://xxx.com
 }
 
-func NewHtmlExtractor(htmlContent string, baseURL string) *HtmlExtractor {
+func NewHtmlExtractor(htmlContent string) *HtmlExtractor {
 	return &HtmlExtractor{
 		tokenizer: html.NewTokenizer(strings.NewReader(htmlContent)),
-		baseURL:   baseURL,
 	}
 }
 
@@ -45,30 +43,15 @@ func (he *HtmlExtractor) ExtractText(ctx context.Context) (string, error) {
 			case html.ErrorToken:
 				err := he.tokenizer.Err()
 				if errors.Is(err, io.EOF) {
-
-					text := strings.Join(strings.Fields(sb.String()), " ")
-					return text, nil
+					return sb.String(), nil
 				}
 				return "", fmt.Errorf("html.ErrorToken. err: %w", err)
 			case html.StartTagToken:
-
 				if depth > 0 {
 					continue
 				}
 
 				token := he.tokenizer.Token()
-				if token.Data == "img" || token.Data == "a" {
-					src := he.getImageAndLinkSRC(token)
-					if src == "" {
-						continue
-					}
-
-					sb.WriteString(" ")
-					sb.WriteString(src)
-					sb.WriteString(" ")
-					continue
-				}
-
 				if ignoreTagMap[token.Data] {
 					depth++
 					currentIgnoreTag = token.Data
@@ -86,26 +69,13 @@ func (he *HtmlExtractor) ExtractText(ctx context.Context) (string, error) {
 					currentIgnoreTag = ""
 				}
 
-			case html.SelfClosingTagToken:
-				token := he.tokenizer.Token()
-				if token.Data == "img" || token.Data == "a" {
-					src := he.getImageAndLinkSRC(token)
-					if src == "" {
-						continue
-					}
-
-					sb.WriteString(" ")
-					sb.WriteString(src)
-					sb.WriteString(" ")
-					continue
-				}
-
 			case html.TextToken:
 				if depth > 0 {
 					continue
 				}
 
 				rawData := he.tokenizer.Token().Data
+
 				text := strings.Join(strings.Fields(rawData), " ")
 				if text == "" {
 					continue
@@ -115,21 +85,4 @@ func (he *HtmlExtractor) ExtractText(ctx context.Context) (string, error) {
 			}
 		}
 	}
-}
-
-func (he *HtmlExtractor) getImageAndLinkSRC(token html.Token) string {
-	if token.Data == "img" || token.Data == "a" {
-		for _, att := range token.Attr {
-			if att.Key == "src" {
-
-				if strings.HasPrefix(att.Val, "https://") || strings.HasPrefix(att.Val, "http://") {
-					return att.Val
-				}
-
-				// htmlをparseしてdomainがない場合は、付け足す
-				return he.baseURL + att.Val
-			}
-		}
-	}
-	return ""
 }
