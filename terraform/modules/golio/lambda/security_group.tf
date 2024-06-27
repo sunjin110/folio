6490e54dadd838178fe7a2050831dfa4448f0c3c
sunjin110 <sunjin110@gmail.com>
2024-06-27 16:09:33 +0800

    feat: [terraform] RDSからNeonのDatasourceに切り替え

--- terraform/modules/golio/lambda/security_group.tf
+++ terraform/modules/golio/lambda/security_group.tf
@@ -24,3 +24,13 @@ resource "aws_security_group_rule" "allow_egress_https" {
   from_port         = 443
   to_port           = 443
 }
+
+resource "aws_security_group_rule" "allow_egress_neon_rdb_access" {
+  security_group_id = aws_security_group.this.id
+  type              = "egress"
+  description       = "allow public neon db access"
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  from_port         = 5432
+  to_port           = 5432
+}
