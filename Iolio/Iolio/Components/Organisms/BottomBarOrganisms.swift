88fbeef07be3809534845f546f6a196e501ffb59
sunjin110 <sunjin110@gmail.com>
2024-08-01 15:25:22 +0900

    feat: aa

---
+++ Iolio/Iolio/Components/Organisms/BottomBarOrganisms.swift
@@ -0,0 +1,18 @@
+//
+//  BottomBarOrganisms.swift
+//  Iolio
+//
+//  Created by 尹舜真 on 2024/08/01.
+//
+
+import SwiftUI
+
+struct BottomBarOrganisms: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BottomBarOrganisms()
+}
--- iolio/iolio/Domain/DomainModel/ArticleTagDomainModel.swift
+++
@@ -1,6 +0,0 @@
-public extension DomainModel {
-    struct ArticleTag {
-        var id: String
-        var name: String
-    }
-}
