12b92cd3db4c2566e20b6e2c8f983cec3a786dc0
sunjin110 <sunjin110@gmail.com>
2024-08-09 17:30:06 +0900

    feat: [Iolioo] 認証認可

--- Iolioo/Iolioo/View/LoginView.swift
+++ Iolioo/Iolioo/View/LoginView.swift
@@ -3,7 +3,7 @@ import GoogleSignIn
 
 struct LoginView: View {
     
-    var authUsecase: Usecase.AuthUsecase
+    let authUsecase: Usecase.AuthUsecase
     
     @Binding var showLogin: Bool
     
@@ -13,12 +13,12 @@ struct LoginView: View {
     
     func handleGoogleSignInOnTap() {
         guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
-                 let rootViewController = windowScene.windows.first(where: { $0.isKeyWindow })?.rootViewController else {
+              let rootViewController = windowScene.windows.first(where: { $0.isKeyWindow })?.rootViewController else {
                print("RootViewController not found.")
                return
-           }
-        
-        GIDSignIn.sharedInstance.signIn(withPresenting: rootViewController) { signInResult, error in
+        }
+
+        GIDSignIn.sharedInstance.signIn(withPresenting: rootViewController as UIViewController) { signInResult, error in
             if error != nil {
                 print("failed google sign in. err: \(error.debugDescription)")
                 return
@@ -36,20 +36,24 @@ struct LoginView: View {
                 }
                 
                 guard let user = user else { print ("user is empty"); return }
+                guard let idToken = user.idToken else {
+                    print("id token is empty")
+                    return
+                }
                 
-                print("id token is \(user.idToken!.tokenString)")
-                print("access token is \(user.accessToken.tokenString)")
-                print("refresh token is \(user.refreshToken.tokenString)")
-                
-//                self.authUsecase.verifyTokenAndStartSession(idToken: <#T##String#>, accessToken: <#T##String#>, refreshToken: <#T##String#>)
-                
-                // TODO golioにid tokenを検証するやつを作る
-                // https://developers.google.com/identity/sign-in/ios/backend-auth?hl=ja
-                
-                
-                showLogin = false
+                Task {
+                    let result = await self.authUsecase.verifyTokenAndStartSession(idToken: idToken.tokenString, accessToken: user.accessToken.tokenString, refreshToken:user.refreshToken.tokenString)
+                    switch result {
+                    case .success(_):
+                        DispatchQueue.main.async {
+                            print("success verify and start session")
+                            self.showLogin = false
+                        }
+                    case .failure(let err):
+                        print("failed authUsecase.verifyTokenAndStartSession. err: \(err)")
+                    }
+                }
             }
-            
         }
     }
     
