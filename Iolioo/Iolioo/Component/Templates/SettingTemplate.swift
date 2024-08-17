0c0360ad7ad4e7924421cd95b69d61b227a6908f
sunjin110 <sunjin110@gmail.com>
2024-08-17 15:15:37 +0900

    feat: [Iolioo] toast導入

--- Iolioo/Iolioo/Component/Templates/SettingTemplate.swift
+++ Iolioo/Iolioo/Component/Templates/SettingTemplate.swift
@@ -1,8 +1,15 @@
 import SwiftUI
+import AlertToast
 
 struct SettingTemplate: View {
 
     var loginOnTap: () -> Void
+    
+    @State
+    private var showTast = false
+    
+    @State
+    private var signInShowTastFlag = false
 
     var body: some View {
         VStack {
@@ -15,6 +22,19 @@ struct SettingTemplate: View {
                     Text("Login").font(.title)
                 })
             Spacer()
+            
+            Button("Show Toast") {
+                showTast = true
+            }
+            Spacer()
+            
+            Button("Sign in Success toast") {
+                signInShowTastFlag = true
+            }
+        }.toast(isPresenting: $showTast) {
+            AlertToast(displayMode: .hud, type: .complete(.green), title: "Hello Show Tast", subTitle: "It's made by library")
+        }.toast(isPresenting: $signInShowTastFlag) {
+            AlertToast(displayMode: .alert, type: .complete(.green), title: "Sign in!")
         }
     }
 }
