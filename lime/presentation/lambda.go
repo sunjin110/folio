51a4e2a0499ef72cf721e3ef00cae4bcbed99055
sunjin110 <sunjin110@gmail.com>
2024-10-23 10:15:39 +0900

    a

--- lime/presentation/lambda.go
+++ lime/presentation/lambda.go
@@ -14,21 +14,19 @@ func GetHandler() (lambdaHandlerFunc func(ctx context.Context, req events.APIGat
 	r := mux.NewRouter()
 
 	slog.Info("routerは作った")
-	r.Get("/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		slog.Info("======= / がリクエストされた")
-
 		slog.Info("base")
 		w.Write([]byte(`{"hello": "lime"}`))
 		w.WriteHeader(http.StatusOK)
 		slog.Info("====== / の処理終わり")
-	})
-	r.Get("/hello").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	}).Methods(http.MethodGet)
+	r.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		slog.Info("hello lime")
 		w.Write([]byte(`{"hello": "lime"}`))
 		w.WriteHeader(http.StatusOK)
 		slog.Info("======== /helloの処理終わり")
-	})
+	}).Methods(http.MethodGet)
 
 	slog.Info("ここまできた")
 	return httpadapter.New(r).ProxyWithContext, nil
